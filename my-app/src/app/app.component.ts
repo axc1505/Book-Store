2db769eed7172db01992443dc6e4e4bfdf11b03e
zorochimaru <rasim.karimli@gmail.com>
2022-04-30 23:33:40 +0400

    finished job

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,74 +1,81 @@
-import { Component, NgModule, OnInit } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-//import { NgModule } from '@angular/core';
-import {books} from './books.json';
+import { Component } from '@angular/core';
+import { Book } from '../models/book.model';
+import booksJson from '../assets/books.json';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
-  title = 'my-app';
-    id=Number;
-    name= String;
-    author = String;
-    date = String;
-    md = String;
+  editMode = false;
+  sortAsc = true;
+  searchedKeyword = '';
+  books: Book[] = booksJson.books;
+  filteredList: Book[] = this.books;
+  book: Book = {
+    id: new Date().getTime(),
+    author: '',
+    name: '',
+    dateAdded: new Date().toString(),
+    dateModified: new Date().toString(),
+  };
+  sortBy(type: 'name' | 'dateAdded') {
+    this.filteredList = this.filteredList.sort((a, b) => {
+      if (a[type] < b[type]) {
+        return this.sortAsc ? 1 : -1;
+      } else return this.sortAsc ? -1 : 1;
+    });
+    this.sortAsc = !this.sortAsc;
+  }
+  onEdit(id: number) {
+    this.editMode = true;
+    const editBook = this.filteredList.find((x) => x.id === id);
+    this.book = editBook!;
+  }
 
+  onSubmit(form: NgForm) {
+    if (this.editMode) {
+      const newEditedBook: Book = {
+        id: this.book.id,
+        name: form.value.book.name,
+        author: form.value.book.author,
+        dateAdded: this.book.dateAdded,
+        dateModified: new Date().toString(),
+      };
+      this.editMode = false;
+      const editBookIndex = this.filteredList.findIndex(
+        (x) => x.id === newEditedBook.id
+      );
+      this.books.splice(editBookIndex, 1, newEditedBook);
+    } else {
+      const newBook: Book = {
+        id: new Date().getTime(),
+        name: form.value.book.name,
+        author: form.value.book.author,
+        dateAdded: new Date().toString(),
+        dateModified: new Date().toString(),
+      };
+      this.books.push(newBook);
+    }
+    form.reset();
+  }
 
+  onDelete(id: number) {
+    this.books = this.books.filter((x) => x.id !== id);
+    this.filteredList = this.filteredList.filter((x) => x.id !== id);
+  }
 
-    
-
-
-
-
-
-  searchedKeyword!: string;
-  books =[{
-    "id": 1,
-    "name": "book 1",
-    "author":"book's author",
-    "dateAdded":"date",
-    "DateModified": "date"
-},{
-  "id": 2,
-  "name": "book 2",
-  "author":"book's author",
-  "dateAdded":"date",
-  "DateModified": "date"
-},{
-  "id": 3,
-  "name": "book 3",
-  "author":"book's author",
-  "dateAdded":"date",
-  "DateModified": "date"
-},{
-  "id": 4,
-  "name": "book 4",
-  "author":"book's author",
-  "dateAdded":"date",
-  "DateModified": "date"
-},{
-  "id": 5,
-  "name": "book 5",
-  "author":"book's author",
-  "dateAdded":"date",
-  "DateModified": "date"
-},
-
-
-
-
-
-
-
-
-]
-
-  constructor(){
- 
-
-
-  }}
+  onSearch(): void {
+    this.filteredList = this.books.filter(
+      (x) =>
+        x.name
+          .toLocaleLowerCase()
+          .includes(this.searchedKeyword.toLowerCase().trim()) ||
+        x.author
+          .toLocaleLowerCase()
+          .includes(this.searchedKeyword.toLowerCase().trim())
+    );
+  }
+}
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,26 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { FormsModule } from '@angular/forms';
-import { UpdateComponent } from './components/update/update.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UpdateComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CommonModule,
-    Ng2SearchPipeModule,
-    FormsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CommonModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-
-
- }
+export class AppModule {}
